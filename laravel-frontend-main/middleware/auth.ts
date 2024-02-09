5d5e82b72ebb9b5a769ff59fe98ca6bd6ad0de92
Arnau Badenas <arnau.badenas.7e6@itb.cat>
2024-02-09 11:49:51 +0100

    Revert "Formatar Dades d'usuari fet"
    This reverts commit 7474064e4a7bbc4f9bbd8b069725360c4548044f.

--- laravel-frontend-main/middleware/auth.ts
+++ laravel-frontend-main/middleware/auth.ts
@@ -1,22 +1,12 @@
 import axios, { AxiosError } from "axios"
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-
-    const {user, initUser} = useAuth()
-    if(user.value == null){
-        user.value = await initUser()
+    try{
+        await axios.get("/api/user")
     }
-    //Si segueix sent null envia a login
-    if(user.value == null){
-        return navigateTo("/login")
+    catch(error){
+        if(error instanceof AxiosError && error.response?.status===401){
+            return navigateTo("/login")
+        }
     }
-    
-    // try{
-    //     await axios.get("/api/user")
-    // }    
-    // catch(error){
-    //     if(error instanceof AxiosError && error.response?.status===401){
-            
-    //     }
-    // }
 })
--- laravel-frontend-main/middleware/guest.ts
+++ laravel-frontend-main/middleware/guest.ts
@@ -1,11 +1,12 @@
 import axios, { AxiosError } from "axios"
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-
-    const {user, initUser} = useAuth()
-    user.value = await initUser()
-    if(user.value != null){
-        return navigateTo("/me")
+    try{
+        const {data:user} = await axios.get("/api/user")
+        if(user){
+            return navigateTo("/me")
+        }
     }
-    
+    catch(error){
+    }
 })
