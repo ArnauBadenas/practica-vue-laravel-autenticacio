a55a3713a351ffe91077d48937259bca74f14f1f
Arnau Badenas <arnau.badenas.7e6@itb.cat>
2024-02-20 19:10:04 +0100

    Manipulació d’errors de validació d’un formulari

--- laravel-frontend-main/pages/login.vue
+++ laravel-frontend-main/pages/login.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { Ref } from 'nuxt/dist/app/compat/vue-demi';
 
 definePageMeta({
@@ -17,30 +17,41 @@ const user:Ref<LoginPayload> = ref({
 
 import {useAuth} from "../composables/useAuth";
 const {login}=useAuth();
-// async function login(user:LoginPayload){
-//   try{
-//     const login = await axios.post("/api/login", user)
-//     console.log(user)
-//     const router = useRouter()
-//     router.replace("/me")
-//   }catch(e){
-//     console.log(e)
-//   }
-// }
 
+// Mostrar validació d'errors al front
+const errors:Ref<{email:string[],password:string[]}>=ref({
+  email:[],
+  password:[]
+})
+
+async function handleLogin(){
+  try{
+    errors.value.email = []
+    errors.value.password = []
+    await login(user.value)
+  }catch(error){
+    if(error instanceof AxiosError && error.response?.status===422){
+      errors.value.email.push(...error.response.data.errors.email)
+      errors.value.password.push(...error.response.data.errors.password)
+      console.log(errors.value)
+    }   
+  }
+}
 </script>
 <template>
   <div class="login">
     <h1>Login</h1>
-    <form @submit.prevent="login(user)">
+    <form @submit.prevent="handleLogin()">
       <label>
         <div>Email</div>
         <input type="text" v-model="user.email"/>
+        <div class="emailErrors">{{ errors.email }}</div>
       </label>
 
       <label>
         <div>Password</div>
         <input type="password" v-model="user.password"/>
+        <div class="passwordErrors">{{ errors.password }}</div>
       </label>
       <button class="btn">Login</button>
     </form>
